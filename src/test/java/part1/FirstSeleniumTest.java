3064f4758af9fd8002f855ef601739c0c6e63dc5
Zee-2000 <zahwaabdelnasser@gmail.com>
2025-08-13 11:16:05 +0300

    first test script

--- src/test/java/part1/FirstSeleniumTest.java
+++ src/test/java/part1/FirstSeleniumTest.java
@@ -2,7 +2,9 @@ package part1;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,17 +16,22 @@ public class FirstSeleniumTest {
     {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://best.aliexpress.com/?gatewayAdapt=glo2ara&browser_redirect=true");
+        driver.get("https://www.saucedemo.com/v1/");
     }
     @AfterClass
-    public void tearDown()
+   public void tearDown()
     {
         //driver.quit();
     }
     @Test
-    public void testSignup()
-    {
-        driver.findElement(By.className("nfm-multiple-container pc-account rtl-account"));
+    public void loginTest() throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement username = driver.findElement(By.name("user-name"));
+        username.sendKeys("problem_user");
+        var password = driver.findElement(By.name("password"));
+        password.sendKeys("secret_sauce");
+        driver.findElement(By.className("btn_action")).click();
+        
     }
 
 
