51fef0ddea354ce6c32a76870df6570434decbaf
Zee-2000 <zahwaabdelnasser@gmail.com>
2025-08-13 11:19:00 +0300

    first test script

--- src/main/java/com/saucedemo/pages/LoginPage.java
+++ src/main/java/com/saucedemo/pages/LoginPage.java
@@ -1,5 +1,10 @@
 package com.saucedemo.pages;
 
+import org.openqa.selenium.By;
+
 public class LoginPage extends BasePage{
+private By usernameField;
+private By passwordField;
+private By errorMessage;
 
 }
